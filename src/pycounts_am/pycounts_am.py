55852f2021e59fcf72e1ffc303e328f3eef3ea39
Adam Morphy <adam.morphy@outlook.com>
2022-01-10 19:04:15 -0800

    Plotting src added

--- src/pycounts_am/pycounts_am.py
+++ src/pycounts_am/pycounts_am.py
@@ -1,6 +1,5 @@
 from collections import Counter
 from string import punctuation
-import matplotlib.pyplot as plt
 
 def load_text(input_file):
     """Load text from a text file and return as a string."""
@@ -21,13 +20,3 @@ def count_words(input_file):
     text = clean_text(text)
     words = text.split()
     return Counter(words)
-
-def plot_words(word_counts, n=10):
-    """Plot a bar chart of word counts."""
-    top_n_words = word_counts.most_common(n)
-    word, count = zip(*top_n_words)
-    fig = plt.bar(range(n), count)
-    plt.xticks(range(n), labels=word, rotation=45)
-    plt.xlabel("Word")
-    plt.ylabel("Count")
-    return fig
